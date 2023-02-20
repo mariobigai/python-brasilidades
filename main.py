c7a4140411a15e12b6b9c774cd4f22e33acfa06d
Mario Henrique Bigai <109158537+mariobigai@users.noreply.github.com>
2023-02-20 18:20:16 -0300

    Adicionando Factory para gerenciar criação de CPF e CNPJ

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from Cpf import Cpf
+from Documento import Documento
 
 cpf = 21154902072
+<<<<<<< HEAD
+cnpj = 12526560000163
+documento = Documento.cria_novo(cnpj)
+
+print(documento)
+=======
 objeto_cpf = Cpf(cpf)
 
 print(objeto_cpf)
+>>>>>>> c23f3412a8ed26d0bdd9b9ae313a1c80c78a3b6e
